38dc2e74a0c79fe0938698090dce741588460632
djspig <cyclops@monstercode.com>
2018-04-15 09:22:20 -0400

    Add current general conference extractor

---
+++ src/extractors/general-conference.js
@@ -0,0 +1,102 @@
+const argv = require('minimist')(process.argv.slice(2));
+const select = require('xpath.js');
+const DOMParser = require('xmldom').DOMParser;
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+const Promise = require('bluebird');
+const request = require('superagent');
+
+const { JSDOM } = require("jsdom");
+
+function extractFootnotes(chpCnt, talk) {
+
+  return Promise.resolve()
+    .then(() => JSDOM.fragment(talk))
+    .then(dom => dom.querySelector('footer.notes > ol > li'))
+    .then(element => {
+      const footnotes = [];
+      while (element) {
+        footnotes.push(element.innerHTML);
+        element = element.nextElementSibling;
+      }
+      return footnotes;
+    })
+    .then(footnotes => Promise.mapSeries(footnotes, (footnote) =>
+      Promise.resolve(JSDOM.fragment(footnote))
+        .then(dom => dom.querySelector('p'))
+        .then(element => element && element.innerHTML)))
+    .then(footnotes => footnotes.map((p, index) => `
+        <a href="#${chpCnt}_${index + 1}" name="${chpCnt}_${index + 1}_b"><sup>${index + 1}<\/sup><\/a>
+        ${p.replace(/(\s+href\s*=\s*(?:"|'))\//ig, "$1https://lds.org/")}`));
+}
+
+function extractParagraphs(chpCnt, talk) {
+  return Promise.resolve()
+    .then(html => JSDOM.fragment(talk))
+    .then(dom => dom.querySelector('div.body-block > p'))
+    .then(element => {
+        const paragraphs = [];
+        while (element) {
+            paragraphs.push(element.innerHTML);
+            element = element.nextElementSibling;
+        }
+        return paragraphs;
+    })
+    .then(paragraphs => {
+      return paragraphs.map(p => p.replace(
+        /<a\s+class\s*=\s*"note-ref"\s+href=\s*"#note(\d+)"\s*>\s*<sup\s+class="marker"\s*>\s*\1\s*<\/sup>\s*<\/a>/igm,
+        `<a href="#${chpCnt}_$1_b" name="${chpCnt}_$1"><sup>$1<\/sup><\/a>`));
+    });
+}
+
+function extractTalk(url, chptIndex) {
+  const basePath = path.join(__dirname, '..', '..', 'tmp');
+
+  return request
+    .get(`https://www.lds.org/${url}`)
+    .query('json')
+    .then(response => JSON.parse(response.res.text))
+  // return Promise.fromCallback(cb => fs.readFile(path.join(basePath, 'turn-on-your-light.pretty.json'), cb))
+    // .then(response => JSON.parse(response))
+    .then(talk => Promise.props({
+      title: _.get(talk, 'subComponents.ldsOrgHead.meta.title'),
+      number: chptIndex,
+      paragraphs: extractParagraphs(chptIndex, _.get(talk, 'subComponents.articleContent.articleContent')),
+      footnotes: extractFootnotes(chptIndex, _.get(talk, 'subComponents.textDrawer.reference')),
+    }));
+}
+
+function extractSession(session, chptIndex) {
+  return Promise.mapSeries(
+    _.get(session, 'subComponents.tile') || [],
+    // _.take(_.get(session, 'subComponents.tile') || [], 1),
+    (talk, index) => extractTalk(talk.link, chptIndex + index)
+  );
+}
+
+const getBookContents = function () {
+    const basePath = path.join(__dirname, '..', '..', 'tmp');
+
+    return request
+      .get('https://www.lds.org/general-conference')
+      .query('lang=eng&json')
+      // .then(response => Promise.fromCallback(cb => fs.writeFile(path.join(basePath, 'test.json'), response.res.text, { encoding: 'utf8' }, cb)))
+    // return Promise.fromCallback(cb => fs.readFile(path.join(basePath, 'test.json'), cb))
+    .then(response => JSON.parse(response.res.text))
+      // .then(response => JSON.parse(response))
+      .then(conf => Promise.props({
+        title: `General Conference - ${conf.title}`,
+        author: "The Church of Jesus Christ of Latter-Day Saints",
+        chapters: Promise.reduce(
+          _.get(conf, 'subComponents.sessions'),
+          (memo, session) => extractSession(session, memo.length)
+            .then((chapters) => memo.concat(chapters)),
+          []
+        )
+      }));
+};
+
+_.assign(module.exports, {
+    getBookContents
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+const argv = require('minimist')(process.argv.slice(2));
 const Handlebars = require('handlebars');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const path = require('path');
 const _ = require('lodash');
+const rimraf = require('rimraf');
 
 const templateDir = path.join(__dirname, 'templates');
+const partialDir = path.join(templateDir, 'partials');
 
 var mkdirp = Promise.promisify(require('mkdirp'));
 
@@ -13,7 +16,7 @@ const { getBookContents } = require('./extractors/signature-books');
 Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
     lvalue = parseFloat(lvalue);
     rvalue = parseFloat(rvalue);
-        
+
     return {
         "+": lvalue + rvalue,
         "-": lvalue - rvalue,
@@ -23,34 +26,67 @@ Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
     }[operator];
 });
 
-Promise.resolve()
-    .then(() => mkdirp(path.join(__dirname, '..', 'build')))
-	.then(() =>
-		// load up the templates
-		Promise.reduce(
-			fs.readdirAsync(templateDir),
-			(memo, item) => {
-				const match = item.match(/^(.*)\.hbs$/);
-				if (match) {
-					const filepath = path.join(templateDir, item);
-					const file = fs.readFileSync(filepath, { encoding: 'utf8' })
-					memo[match[1]] = Handlebars.compile(file);
-				}
-				return memo;
-			},
-			{}
-		)
-	)
-	.then((templates) => getBookContents()
-        .then((book) => {
-            debugger;
-            return Promise.props(_.transform(templates, (memo, value, key) => memo[key] = templates[key](book), {}));
-        })
-    )
-	.then(results => { 
-        debugger; 
-        return Promise.each(_.keys(results), (key) => fs.writeFileAsync(path.join(__dirname, '..', 'build', key), results[key]))
-    });
+function prepareBuildPath() {
+  const buildPath = path.join(__dirname, '..', 'build');
+
+  return Promise.fromCallback(cb => rimraf(buildPath, cb))
+    .then(() => mkdirp(buildPath))
+    .then(() => buildPath);
+}
+
+function buildTemplates() {
+  // load up the templates
+	return Promise.reduce(
+		fs.readdirAsync(templateDir),
+		(memo, item) => {
+			const match = item.match(/^(.*)\.hbs$/);
+			if (match) {
+				const filepath = path.join(templateDir, item);
+				const file = fs.readFileSync(filepath, { encoding: 'utf8' })
+				memo[match[1]] = Handlebars.compile(file);
+			}
+			return memo;
+		},
+		{}
+  );
+}
+
+function registerPartials() {
+  // register all the partial buildTemplates
+  return Promise.reduce(
+    fs.readdirAsync(partialDir),
+    (memo, item) => {
+      const match = item.match(/^(.*)\.hbs$/);
+			if (match) {
+				const filepath = path.join(partialDir, item);
+				const file = fs.readFileSync(filepath, { encoding: 'utf8' })
+				memo[match[1]] = file;
+			}
+			return memo;
+    },
+    {}
+  ).then(partials => {
+    return Handlebars.registerPartial(partials);})
+}
+
+function getBuilder() {
+  if (!_.isArray(argv._) || argv._.length <= 0) {
+    return Promise.reject('Must specify extractor');
+  }
+
+  return Promise.resolve()
+    .then(() => require(path.join(process.cwd(), _.first(argv._))))
+    .then(builder => !_.isFunction(builder.getBookContents) ? Promise.reject('Missing "getBookContents()"') : builder);
+}
+
+return Promise.props({
+  buildDir: prepareBuildPath(),
+  templates: registerPartials().then(() => buildTemplates()),
+  builder: getBuilder(),
+})
+	.then(({ templates, builder, buildDir }) => builder.getBookContents()
+    .then(book => Promise.props(_.transform(templates, (memo, value, key) => memo[key] = templates[key](book), {})))
+    .then(results => Promise.each(_.keys(results), (key) => fs.writeFileAsync(path.join(buildDir, key), results[key])))
+  );
 
 // const book = hbs.compile(source);
-
